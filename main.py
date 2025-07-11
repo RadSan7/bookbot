24cc0e8e46ad80e6a038b01491248bd7ea8092ff
RadSan7 <radek.san7@icloud.com>
2025-07-11 03:23:04 +0200

    C: Letters count and sort

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from stats import count_words
+from stats import count_words, get_sorted_characters
 
 with open("books/frankenstein.txt") as book:
     text = book.read()
 
-num_words = count_words(text)
+num_words, letters = count_words(text)
 
-print(f"{num_words} words found in the document")
+sorted_letters = get_sorted_characters(letters)
 
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at books/frankenstein.txt")
+print("----------- Word Count ----------")
+print(f"Found {num_words} total words")
+print("--------- Character Count -------")
+for item in sorted_letters:
+    print(f"{item['char']}: {item['num']}")
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,5 +1,28 @@
 def count_words(text):    
+
     words = text.split()
     num_words = len(words)
-    return num_words  
+
+    letters = {}
+
+    for i in range(len(text)):
+        char = text[i]
+        if char.isalpha():
+            char = char.lower()
+        letters[char] = letters.get(char, 0) + 1
+        
+    return num_words, letters
+
+def sort_on(item):
+    return item["num"]
+
+def get_sorted_characters(char_dict):
+    sorted_list = []
+
+    for char, count in char_dict.items():
+        if char.isalpha():  # pomijamy np. przecinki, spacje itp.
+            sorted_list.append({"char": char, "num": count})
+
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
     
