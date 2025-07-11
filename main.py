97c4ef376f0ab3300b0b4ccc0061485408960b5d
RadSan7 <radek.san7@icloud.com>
2025-07-11 14:12:01 +0200

    C: sys.argv added

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+from sys import argv
 from stats import count_words, get_sorted_characters
 
-with open("books/frankenstein.txt") as book:
-    text = book.read()
+if len(argv) == 1:
+    print("Usage: python3 main.py <path_to_book>")
+    exit(1)
 
+with open(argv[1]) as book:
+    text = book.read()
 num_words, letters = count_words(text)
 
 sorted_letters = get_sorted_characters(letters)
 
 print("============ BOOKBOT ============")
-print(f"Analyzing book found at books/frankenstein.txt")
+print(f"Analyzing book found at {argv[1]}")
 print("----------- Word Count ----------")
 print(f"Found {num_words} total words")
 print("--------- Character Count -------")
